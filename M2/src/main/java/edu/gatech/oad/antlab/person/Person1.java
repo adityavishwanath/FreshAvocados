ad6a2ebc54da087c880cb8b7266839de680510a9
Brody Johnstone <brodyjackj@gatech.edu>
2016-01-26 09:26:09 -0500

    Added Person1.java

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -3,22 +3,27 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 1
  *  returns their name and a
- *  modified string 
- *  
- *  @author Bob
+ *  modified string
+ *
+ *  @author Bob and Brody Johnstone
  *  @version 1.1
  */
 public class Person1 {
-  /** Holds the persons real name */
-  private String name;
+
+    public static void main(String[] args) {
+        Person1 brody = new Person1("Brody");
+        System.out.println(brody.calc("gtg123b"));
+    }
+    /** Holds the persons real name */
+    private String name;
   	/**
 	 * The constructor, takes in the persons
 	 * name
 	 * @param pname the person's real name
 	 */
-  public Person1(String pname) {
-    name = pname;
-  }
+    public Person1(String pname) {
+        name = pname;
+    }
   	/**
 	 * This method should take the string
 	 * input and return its characters rotated
@@ -30,20 +35,21 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+	    //Person 1 put your implementation here
+	    String majority = input.substring(2);
+        String returnString = input.substring(0, 2) + majority;
+        return returnString;
 	}
-	
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
-	  return name + calc(input);
+	    return name + calc(input);
 	}
-
 }

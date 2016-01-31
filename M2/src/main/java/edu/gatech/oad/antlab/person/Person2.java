bf5932fc327ee3d1b44a5c0c45f1563bef63847b
Brody Johnstone <brodyjackj@gatech.edu>
2016-01-31 16:38:07 -0500

    Git is telling me to commit i dont know why

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -3,7 +3,7 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 2
  *  returns their name and a
- *  modified string 
+ *  modified string
  *
  * @author Bob and Aditya Vishwanath
  * @version 1.1
@@ -43,7 +43,7 @@ public class Person2 {
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {

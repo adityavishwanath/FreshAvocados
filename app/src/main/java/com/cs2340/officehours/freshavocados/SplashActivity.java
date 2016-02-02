dee0297d8695088ded009c57155521062533882e
Brandon Manuel <brandon-manuel@live.com>
2016-02-02 09:50:29 -0500

    Login Button now launches Login Screen

--- app/src/main/java/com/cs2340/officehours/freshavocados/SplashActivity.java
+++ app/src/main/java/com/cs2340/officehours/freshavocados/SplashActivity.java
@@ -1,10 +1,12 @@
 package com.cs2340.officehours.freshavocados;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 
 public class SplashActivity extends Activity {
@@ -38,7 +40,8 @@ public class SplashActivity extends Activity {
     }
 
     public void onClickLogin(View v) {
-
+        Button button = (Button) v;
+        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
     }
 
     public void onClickRegister(View v) {

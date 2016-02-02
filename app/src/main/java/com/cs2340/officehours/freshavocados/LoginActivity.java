27278a7f22550eb2d1456705e3b0efaf08d5c5ed
Brody Johnstone <brodyjackj@gatech.edu>
2016-02-02 10:37:36 -0500

    Added a confirm button to LoginActivity, with a nice Toast

--- app/src/main/java/com/cs2340/officehours/freshavocados/LoginActivity.java
+++ app/src/main/java/com/cs2340/officehours/freshavocados/LoginActivity.java
@@ -2,6 +2,8 @@ package com.cs2340.officehours.freshavocados;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity {
 
@@ -9,6 +11,12 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onClickConfirmLogin(View v) {
+        Toast toast = Toast.makeText(getApplicationContext(), "Temporary toast!", Toast.LENGTH_SHORT);
+        toast.show();
+
     }
 
 }

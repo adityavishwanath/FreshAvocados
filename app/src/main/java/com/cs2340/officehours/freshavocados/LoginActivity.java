b1a905252d1a45e17f2cfe11e0a9005fc0fb2252
Brody Johnstone <brodyjackj@gatech.edu>
2016-02-02 10:45:45 -0500

    Added a cancel button to Login Activity

--- app/src/main/java/com/cs2340/officehours/freshavocados/LoginActivity.java
+++ app/src/main/java/com/cs2340/officehours/freshavocados/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.cs2340.officehours.freshavocados;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
@@ -17,6 +18,10 @@ public class LoginActivity extends Activity {
         Toast toast = Toast.makeText(getApplicationContext(), "Temporary toast!", Toast.LENGTH_SHORT);
         toast.show();
 
+    }
+
+    public void onClickCancelLogin(View v) {
+        startActivity(new Intent(getApplicationContext(), SplashActivity.class));
     }
 
 }

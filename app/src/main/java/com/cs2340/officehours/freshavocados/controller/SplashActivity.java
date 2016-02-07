2724d75f0718ae1d8ff5d3f85911aece46b12cc7
Brody Johnstone <brodyjackj@gatech.edu>
2016-02-07 12:53:30 -0500

    added dummy user, user/pass

--- app/src/main/java/com/cs2340/officehours/freshavocados/controller/SplashActivity.java
+++ app/src/main/java/com/cs2340/officehours/freshavocados/controller/SplashActivity.java
@@ -36,6 +36,8 @@ public class SplashActivity extends Activity {
         uM.addUser("Brandon", "Manuel", "brandon", "password");
         uM.addUser("Pranathi", "Tupakula", "pranathi", "password");
         uM.addUser("Vagdevi", "Kondeti", "vagdevi", "password");
+        //just a dummy user/pass as per the requirement of M3
+        uM.addUser("Temporary", "M3Account", "user", "pass");
 
         Log.d("SplashActivity", "CREATED THE TEMPORARY USERS!");
     }

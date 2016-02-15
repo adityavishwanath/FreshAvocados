e0772632c5ffc93552c9d409a983e5bc40b1ec84
Brody Johnstone <brodyjackj@gatech.edu>
2016-02-15 07:23:28 -0500

    Added UI for EditProfile activity

--- app/src/main/java/com/cs2340/officehours/freshavocados/controller/EditProfileActivity.java
+++ app/src/main/java/com/cs2340/officehours/freshavocados/controller/EditProfileActivity.java
@@ -2,6 +2,8 @@ package com.cs2340.officehours.freshavocados.controller;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.EditText;
 
 import com.cs2340.officehours.freshavocados.R;
 
@@ -11,6 +13,11 @@ public class EditProfileActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+    }
+
+    public void onClickUpdate(View v) {
+        EditText newPass = (EditText) findViewById(R.id.new_password);
+        EditText confirmPass = (EditText) findViewById(R.id.confirm_password);
     }
 
 }

70fe8a5d4e36e4e88e17496d3106f2f803859cee
Brody Johnstone <brodyjackj@gatech.edu>
2016-02-15 07:47:14 -0500

    Finished EditProfile activity, changed currentUser to null when logout button is pressed

--- app/src/main/java/com/cs2340/officehours/freshavocados/controller/EditProfileActivity.java
+++ app/src/main/java/com/cs2340/officehours/freshavocados/controller/EditProfileActivity.java
@@ -1,13 +1,20 @@
 package com.cs2340.officehours.freshavocados.controller;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.os.Vibrator;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.cs2340.officehours.freshavocados.R;
 
 public class EditProfileActivity extends Activity {
+
+    Toast profileUpdatedToast;
+    Toast passwordsNotMatched;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +24,30 @@ public class EditProfileActivity extends Activity {
 
     public void onClickUpdate(View v) {
         EditText newPass = (EditText) findViewById(R.id.new_password);
-        EditText confirmPass = (EditText) findViewById(R.id.confirm_password);
+        EditText confirmPass = (EditText) findViewById(R.id.confirm_new_password);
+
+        if (newPass.getText().toString().equals(confirmPass.getText().toString())) {
+            LoginActivity.currentUser.setPass(newPass.getText().toString());
+            if (profileUpdatedToast == null) {
+                profileUpdatedToast = Toast.makeText(getApplicationContext(),
+                        "Profile Updated!", Toast.LENGTH_SHORT);
+            }
+            profileUpdatedToast.show();
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        } else {
+            if (passwordsNotMatched == null) {
+                passwordsNotMatched = Toast.makeText(getApplicationContext(),
+                        "Passwords did not match. Please try again.", Toast.LENGTH_SHORT);
+            }
+            passwordsNotMatched.show();
+        }
+        Vibrator a = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        a.vibrate(50);
     }
 
+    public void onClickCancelEditProfile(View v) {
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        Vibrator a = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        a.vibrate(50);
+    }
 }
--- app/src/main/java/com/cs2340/officehours/freshavocados/controller/MainActivity.java
+++ app/src/main/java/com/cs2340/officehours/freshavocados/controller/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends Activity {
     }
 
     public void onClickLogout(View v) {
+        LoginActivity.currentUser = null;
         startActivity(new Intent(getApplicationContext(), SplashActivity.class));
         Vibrator a = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         a.vibrate(50);
@@ -25,5 +26,7 @@ public class MainActivity extends Activity {
 
     public void onClickEdit(View view) {
         startActivity(new Intent(getApplicationContext(), ViewProfileActivity.class));
+        Vibrator a = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        a.vibrate(50);
     }
 }
